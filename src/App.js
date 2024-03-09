7aafc79596b52bbc8ab0aa1f1c0e895ccba02eaa
sann <psang4845@gmail.com>
2024-03-09 15:58:33 +0700

    fixed image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navigation from './customer/Components/Navbar/Navigation';
 import CustomerRoutes from './Routers/CustomerRoutes';
 import AdminRoutes from './Routers/AdminRoutes';
